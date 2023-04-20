c73e9afcc9c02199b34ad78be254b326025646f3
Breno Felix <brenodev.felix@gmail.com>
2023-04-20 13:41:23 -0300

    :lipstick:	feat: Login

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-
+    <SgusLogin/>
   </div>
 </template>
 
 <script>
+import SgusLogin from './components/SgusLogin.vue'; 
+
 export default {
   name: 'App',
   components: {
-
+    SgusLogin 
   }
 }
 </script>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap');
+@import url('https://fonts.cdnfonts.com/css/gacor-personal-use');
 </style>
---
+++ src/components/SgusLogin.vue
@@ -0,0 +1,129 @@
+<template>
+    <div>
+        <div class="container">
+            <h1> SGUS </h1>
+            <h2> Sistema Gerenciador de Usuários</h2>
+            <form action="#" method="post">
+                <label for="username">Usuário:</label>
+                <input type="text" id="username" name="username" required>
+                <label for="password">Senha:</label>
+                <input type="password" id="password" name="password" required>
+                <p class="forgot-password"><a href="#">Esqueceu sua senha?</a></p>
+                <button type="submit">Acessar →</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    color: #000000;
+    box-sizing: border-box;
+}
+
+div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.container {
+    width: 36%;
+    height: 32.25%;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+h1 {
+    width: 207px;
+    height: 90px;
+    font-family: 'Gacor Personal Use';
+    font-weight: 400;
+    font-size: 48px;
+    line-height: 90px;
+    color: #4763E4;
+    margin-bottom: 4.2vh;
+}
+
+h2 {
+    width: 319px;
+    height: 24px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+    margin-bottom: 3.9vh;
+}
+
+label {
+    width: 58px;
+    height: 19px;
+    font-family: 'Inter';
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    margin-bottom: 0.8vh;
+}
+
+form p {
+    text-align: center;
+}
+
+p {
+    width: 100%;
+    height: 19px;
+    font-family: 'Inter';
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    margin-bottom: 1.5vh;
+}
+
+button {
+    width: 520px;
+    height: 48px;
+    background: #4763E4;
+    border-radius: 12px;
+    border: none;
+    cursor: pointer;
+
+    font-family: 'Inter';
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #FFFFFF;
+}
+
+input {
+    width: 520px;
+    height: 49px;
+    border-radius: 12px;
+    margin-bottom: 1.2vh;
+    border: 1px solid #E4E4E7;
+
+    font-family: 'Inter';
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    padding-left: 1.1vw;
+}
+
+input:focus {
+    border: 1px solid #5C73DB;
+}
+</style>

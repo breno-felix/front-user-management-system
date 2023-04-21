d506d3a00862bc53f723239b7c0e754304d144bc
Breno Felix <brenodev.felix@gmail.com>
2023-04-21 10:50:53 -0300

    :lipstick:	feat: App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
-    <SgusLogin/>
+    <SgusHeader @change-component="changeComponent" />
+    <SgusSection :current-component="currentSectionComponent" @change-component="changeComponent"/>
   </div>
 </template>
 
 <script>
-import SgusLogin from './components/SgusLogin.vue'; 
+import SgusHeader from './components/SgusHeader.vue'
+import SgusSection from './components/SgusSection.vue';
 
 export default {
   name: 'App',
   components: {
-    SgusLogin 
+    SgusHeader,
+    SgusSection
+  },
+  data() {
+    return {
+      currentSectionComponent: 'SgusSectionListUser'
+    }
+  },
+  methods: {
+    changeComponent(value) {
+      let component;
+      switch (value) {
+        case 'list-users':
+        default:
+          component = 'SgusSectionListUser';
+          break;
+
+        case 'register-users':
+          component = 'SgusSectionRegisterUser';
+          break;
+      }
+      this.currentSectionComponent = component;
+    }
   }
 }
 </script>
@@ -18,4 +42,17 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap');
 @import url('https://fonts.cdnfonts.com/css/gacor-personal-use');
+
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  color: #000000;
+  box-sizing: border-box;
+  list-style: none;
+}
+
+body {
+  background: #FAFAFA;
+}
 </style>

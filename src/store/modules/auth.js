838c608d34ff5e0f568d3f87a287f55bce724452
Breno Felix <brenodev.felix@gmail.com>
2023-04-22 13:37:39 -0300

    :construction: add userRole to auth store.

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
+import jwt from 'jsonwebtoken';
 
 export default {
     state: {
         token: '',
         isAuthenticated: !!localStorage.getItem('token'),
+        userRole: null
     },
     getters: {
         getToken(state) {
@@ -11,12 +13,18 @@ export default {
         },
         getIsAuthenticated(state) {
             return state.isAuthenticated;
+        },
+        getUserRole(state) {
+            return state.userRole;
         }
     },
     mutations: {
         SET_TOKEN(state, token) {
-            state.token = token
+            state.token = token;
         },
+        SET_USER_ROLE(state, role) {
+            state.userRole = role;
+        }
     },
     actions: {
         login({ commit }, credentials) {
@@ -25,12 +33,21 @@ export default {
                     .then(response => {
                         const token = response.data.accessToken
                         commit('SET_TOKEN', token)
-                        localStorage.setItem('token', token)
+                        localStorage.setItem('token', token);
+
+                        const {role} = jwt.decode(token)
+                        commit('SET_USER_ROLE', role)
+                        localStorage.setItem('userRole', role);
+
                         resolve(response)
                     })
                     .catch(error => {
                         commit('SET_TOKEN', '')
                         localStorage.removeItem('token')
+
+                        commit('SET_USER', null)
+                        localStorage.removeItem('user')
+
                         reject(error)
                     })
             })

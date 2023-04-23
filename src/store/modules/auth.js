5fd2f93573add2fdfe20601f0fe75d92480900c0
Breno Felix <brenodev.felix@gmail.com>
2023-04-23 13:12:36 -0300

    :recycle:	refactor: improve auth store.

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,44 +3,16 @@ import jwt from 'jsonwebtoken';
 
 export default {
     state: {
-        token: '',
         isAuthenticated: false,
-        userRole: null,
-        userName: null,
-        userEmail: null
     },
     getters: {
-        getToken(state) {
-            return state.token;
-        },
         getIsAuthenticated(state) {
             return state.isAuthenticated;
-        },
-        getUserRole(state) {
-            return state.userRole;
-        },
-        getUserName(state) {
-            return state.userName;
-        },
-        getUserEmail(state) {
-            return state.userEmail;
         }
     },
     mutations: {
-        SET_TOKEN(state, token) {
-            state.token = token;
-        },
         SET_IS_AUTHENTICATED(state, isAuthenticated) {
             state.isAuthenticated = isAuthenticated;
-        },
-        SET_USER_ROLE(state, role) {
-            state.userRole = role;
-        },
-        SET_USER_NAME(state, name) {
-            state.userName = name;
-        },
-        SET_USER_EMAIL(state, email) {
-            state.userEmail = email;
         }
     },
     actions: {
@@ -49,29 +21,25 @@ export default {
                 axios.post('http://localhost:3010/auth/login', credentials)
                     .then(response => {
                         const token = response.data.accessToken
-                        commit('SET_TOKEN', token)
                         localStorage.setItem('token', token);
 
                         commit('SET_IS_AUTHENTICATED', true)
 
                         const { role, name, email } = jwt.decode(token)
-                        commit('SET_USER_ROLE', role)
                         localStorage.setItem('userRole', role);
-                        commit('SET_USER_NAME', name)
-                        commit('SET_USER_EMAIL', email)
+                        localStorage.setItem('userName', name);
+                        localStorage.setItem('userEmail', email);
 
                         resolve(response)
                     })
                     .catch(error => {
-                        commit('SET_TOKEN', '')
                         localStorage.removeItem('token')
 
                         commit('SET_IS_AUTHENTICATED', false)
 
-                        commit('SET_USER_ROLE', null)
                         localStorage.removeItem('userRole')
-                        commit('SET_USER_NAME', null)
-                        commit('SET_USER_EMAIL', null)
+                        localStorage.removeItem('userName')
+                        localStorage.removeItem('userEmail')
 
                         reject(error)
                     })
@@ -85,15 +53,14 @@ export default {
                         'Authorization': `Bearer ${token}`
                     }
                 }).then(() => {
-                    commit('SET_TOKEN', '')
                     localStorage.removeItem('token')
 
                     commit('SET_IS_AUTHENTICATED', false)
 
-                    commit('SET_USER_ROLE', null)
                     localStorage.removeItem('userRole')
-                    commit('SET_USER_NAME', null)
-                    commit('SET_USER_EMAIL', null)
+                    localStorage.removeItem('userName')
+                    localStorage.removeItem('userEmail')
+
                     resolve()
                 }).catch(error => {
                     console.log(error)
